c47fa40c9135709fb7bc4573de23397324c55f13
indynet <indynet00@gmail.com>
2025-08-28 19:38:19 -0500

    setup js_of_ocaml and footer

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,16 @@
-let () = print_endline "Hello, World!"
+open Stdlib.Printf
+open Js_of_ocaml
+open Util
+
+let set_footer () = use_elem "c" @@ fun x ->
+    let t = sprintf
+        ("(c) %i All Rights Reserved")
+        curr_year
+        in
+
+    x##.innerHTML := Js.string t
+    ;;
+
+let () = on_load @@ fun () ->
+    set_footer ()
+    ;;
---
+++ bin/util.ml
@@ -0,0 +1,23 @@
+(* A utility for working with Js_of_ocaml
+ *
+ * this module does not contain anything
+ * that pertains to indycave.xyz
+ *)
+
+open Js_of_ocaml
+
+(* infallibly use an element by its id *)
+let use_elem id f =
+    match Dom_html.getElementById_opt id with
+    |   Some x -> f x
+    |   None   -> ()
+    ;;
+
+(* use browser to get current year *)
+let curr_year = new%js Js.date_now##getFullYear ;;
+
+(* dom handler ignoring event *)
+let on_fire f = Dom_html.handler @@ fun _ -> f (); Js._true ;;
+
+(* callback when dom loads *)
+let on_load f = Dom_html.window##.onload := on_fire f ;;
